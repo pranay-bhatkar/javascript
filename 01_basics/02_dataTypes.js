1c602675af337da29b11d5d0ce89d0960e62df98
pranay-bhatkar <pranaybhatkar81@gmail.com>
2025-12-09 23:28:06 +0530

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+/*
+ * Topic: Data Types
+ */
+
+"use strict"; // treat all JS as newer version
+
+// alert(3 + 3) // we are using the nodejs not browser
+
+let name = "pranay";
+let age = 23;
+let isLoggedIn = false;
+
+/*
+ * ========== primitive types ==========
+ * number => 2
+ * bigInt
+ * string => ""
+ * boolean => true | false
+ * null => 
+    standalone value => intentionaly assigned null      
+    ex => let state = null
+ * undefined => value declared but not assigned
+ * symbol = unique
+ *
+ *
+ *
+ * ========== Non primitive types ==========
+ * object
+ *
+ */
+
+console.log(typeof "hitest"); // tells the type
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
