8405bf458baf75eb4193b5da9b164985882c0f79
pranay-bhatkar <pranaybhatkar81@gmail.com>
2025-12-09 23:57:30 +0530

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,10 @@
 let score = "false";
 
-console.log(typeof score);
-console.log(typeof score);
+// console.log(typeof score);
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber); // NaN -> not a number
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // NaN -> not a number
 
 /**
     conversion to number
@@ -20,7 +19,7 @@ console.log(valueInNumber); // NaN -> not a number
 
 let isLoggedIn = "";
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 /**
     1 => true
@@ -34,5 +33,42 @@ console.log(booleanIsLoggedIn);
 let someNumber = undefined;
 let stringNumber = String(someNumber);
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+/*
+ * =============== OPERATIONS ===============
+ */
+
+let value = 3;
+let negValue = -value;
+console.log(negValue);
+
+console.log(2 + 3);
+console.log(2 - 3);
+console.log(2 * 3);
+console.log(2 ** 3);
+console.log(2 / 3); // quotient
+console.log(2 % 3); // remainder
+
+let str1 = "hello";
+let str2 = " pranay";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32
+
+console.log(((3 + 4) * 5) % 3);
+
+console.log(+true);
+console.log(+false);
+
+/**
+postfix
+prefix
+from the mdm doc
+*/
