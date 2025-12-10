d53f44b95c2f92c07dbe8765a489d085c2a0efce
pranay-bhatkar <pranaybhatkar81@gmail.com>
2025-12-10 22:02:47 +0530

    comparions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+/*
+console.log("02" > 1);
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); // in this case null -> 0
+console.log(null <= 0); // in this case null -> 0
+console.log(null < 0);
+*/
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+/*
+ * avoid the this comparison with null and undefined
+ */
+// === strickly check -> data type + value
