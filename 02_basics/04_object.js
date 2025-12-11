fce93942f9cdaa27a7ce7848d6a92ad75a60d5b9
pranay-bhatkar <pranaybhatkar81@gmail.com>
2025-12-11 23:02:58 +0530

    object part two

---
+++ 02_basics/04_object.js
@@ -0,0 +1,70 @@
+/*
+ * Tpoic : Object part 2
+ */
+
+// const tinderUser = new Object(); -> singletone object
+
+const tinderUser = {}; // -> non singleton object
+
+tinderUser.id = "13edc";
+tinderUser.name = "sammy";
+tinderUser.isLoggedIn = true;
+
+// console.log(tinderUser);
+
+const regularUser = {
+  email: "ab@ab.com",
+  fullname: {
+    userfullname: {
+      fname: "pranay",
+      lname: "bhatkar",
+    },
+  },
+};
+
+console.log(regularUser.fullname?.userfullname.fname); //optional chaining
+
+const obj1 = {
+  1: "a",
+  2: "b",
+};
+const obj2 = {
+  3: "a",
+  4: "b",
+};
+// const obj3 = { obj1, obj2 };
+// const obj3 = Object.assign({}, obj1, obj2);
+
+const obj3 = { ...obj1, ...obj2 };
+console.log(obj3);
+
+const users = [
+  {
+    id: 1,
+    name: "a",
+  },
+  {
+    id: 2,
+    name: "b",
+  },
+  {
+    id: 3,
+    name: "c",
+  },
+  {
+    id: 4,
+    name: "d",
+  },
+  {
+    id: 5,
+    name: "e",
+  },
+];
+
+console.log(users[1].name);
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+console.log(tinderUser.hasOwnProperty("isLoggedIn"));
